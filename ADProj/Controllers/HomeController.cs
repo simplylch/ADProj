7f59d64a394fc9e719188a6fad907863aaa10555
kaylene22 <kaylenenkl@gmail.com>
2020-08-13 00:39:57 +0800

    test

--- ADProj/Controllers/HomeController.cs
+++ ADProj/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace ADProj.Controllers
     public class HomeController : Controller
     {
         //test commit and push 
+        //test test
 
         private readonly ILogger<HomeController> _logger;
 
